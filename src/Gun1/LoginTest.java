fd058ebb254450bdf623fb3b7c8a90fdc18827b7
Msı <berkgultekin66@gmail.com>
2025-02-27 20:33:39 +0300

    Yeni Login testi yapıldı

--- src/Gun1/LoginTest.java
+++ src/Gun1/LoginTest.java
@@ -4,8 +4,9 @@ public class LoginTest {
     public static void main(String[] args) {
 
         System.out.println("yeni Login Test");
-        System.out.println("Login Test");
-        System.out.println("Sepet testi");
+
+        System.out.println("Sepet testi 3");
+        System.out.println("Yeni login testi yapıldı ");
 
     }
 }
