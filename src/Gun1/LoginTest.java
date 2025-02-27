2665ee0862892615c305c6b10b6df991acaa3748
Msı <berkgultekin66@gmail.com>
2025-02-27 20:24:14 +0300

    Login Test Aşama 1

---
+++ src/Gun1/LoginTest.java
@@ -0,0 +1,10 @@
+package Gun1;
+
+public class LoginTest {
+    public static void main(String[] args) {
+
+        System.out.println("Yeni Login Test ");
+
+        System.out.println("Login Test 1");
+    }
+}
